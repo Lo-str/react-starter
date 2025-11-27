857318a555c80d368432208e56dbbac2a136836e
LoS <lostreit@pm.me>
2025-11-27 14:19:28 +0100

    test

--- react-starter/src/App.jsx
+++ react-starter/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home.jsx"
-import About from "./pages/About/About.jsx"
+import Home from "./pages/Home/Home.js"
+import About from "./pages/About/About.js"
 
 import Navbar from "./components/Navbar/Navbar.jsx"
 import Header from "./components/Header/Header.jsx"
