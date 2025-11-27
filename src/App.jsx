e191276e3711add7f6171e0c44edf1a14b166c08
LoS <lostreit@pm.me>
2025-11-27 14:11:39 +0100

    removed dead code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx"
+import About from "./pages/About/About.jsx"
 
+import Navbar from "./components/Navbar/Navbar.jsx"
+import Header from "./components/Header/Header.jsx"
 function App() {
-
   return (
     <>
+      <BrowserRouter>
+      <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
 
-export default App
+export default App;
