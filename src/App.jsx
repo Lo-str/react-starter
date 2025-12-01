8e729a8f2c02f7afffd571c5e0cb3f1546c9f85f
LoS <lostreit@pm.me>
2025-12-01 13:33:44 +0100

    more structure changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // App
 import {Route, Routes} from 'react-router-dom'
 import './styles/layout/app.scss'
@@ -10,19 +9,11 @@ import Footer from './components/Footer/Footer.jsx'
 
 // Pages
 import Home from './pages/Home/Home.jsx'
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home.jsx"
-import About from "./pages/About/About.jsx"
->>>>>>> 8227bbdcd657fb42dd01bb1e93e12a1e635221d8
 
-import Navbar from "./components/Navbar/Navbar.jsx"
-import Header from "./components/Header/Header.jsx"
 function App() {
   return (
     // Template structure
     <>
-<<<<<<< HEAD
       <div className="app">
         <div className="app__wrap">
 
@@ -46,15 +37,6 @@ function App() {
 
         </div>
       </div>
-=======
-      <BrowserRouter>
-      <Navbar></Navbar>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
->>>>>>> 8227bbdcd657fb42dd01bb1e93e12a1e635221d8
     </>
   )
 }
